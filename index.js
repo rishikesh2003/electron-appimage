7463cea5f1d337360fffc75647f639730d6ebd3e
Rishikesh <rkesh2003@gmail.com>
2022-09-17 18:38:30 +0530

    added icon for app

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { app, BrowserWindow } = require("electron");
 app.on("ready", () => {
   const win = new BrowserWindow({
     autoHideMenuBar: true,
+    icon: __dirname + "/icon.png",
   });
   win.maximize();
   win.loadURL("https://notion.so");
